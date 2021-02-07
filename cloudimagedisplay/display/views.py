354482a7fb58a18318f74f9834e9b6ff05021a1e
Benoît Valle <benoit.valleea@hotmail.fr>
2021-02-07 17:19:52 +0100

    adjust views.py

--- cloudimagedisplay/display/views.py
+++ cloudimagedisplay/display/views.py
@@ -8,21 +8,16 @@ def index(request):
     # Retrieve the list of existing buckets
     s3 = boto3.client('s3')
     response = s3.list_buckets()
+    print("response is")
+    print(response)
 
+    print("=======")
     # Output the bucket names
     print('Existing buckets:')
     for bucket in response['Buckets']:
         print(bucket)
 
-    # conn = S3Connection('AKIAIMOTKO2QJIH4LOPA', 'M8AR4oVL/8hYS9bXqbWQ9alCot6UWfWHwFfTzXDZ')
-    # bucket = conn.get_bucket('arn:aws:s3:::to-do-list-cloud-images', validate=False)
-    #
-    # for key in bucket.list(prefix='dir-in-bucket'):
-    #     print (key.name.encode('utf-8'))
-
-    bucket = "oui"
-
-    context = {'bucket': bucket}
+    context = {'bucket': response['Buckets']}
     return render(request, "home.html", context)
 
 def display_view(request, url):
